eb87019b9566d5725ac0ba6820c4cd82de502210
Your Name <you@example.com>
2023-03-01 23:22:54 -0300

    1000

--- src/main/java/com/portafolio/mgb/Controller/CProyectos.java
+++ src/main/java/com/portafolio/mgb/Controller/CProyectos.java
@@ -5,53 +5,8 @@
 package com.portafolio.mgb.Controller;
 
 
-
-import com.portafolio.mgb.Entity.Proyectos;
-import com.portafolio.mgb.Security.Controller.Mensaje;
-
-import com.portafolio.mgb.Service.SProyectos;
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-
-
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/proyectos")
-@CrossOrigin(origins = "https://portafoliobacklws.onrender.com")
 public class CProyectos {
-     @Autowired
-    SProyectos sProyectos;
-     
-         @GetMapping("/lista")
-    public ResponseEntity<List<Proyectos>> list(){
-        List<Proyectos> list = sProyectos.list();
-        return new ResponseEntity(list, HttpStatus.OK);
     
-    }
-     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") int id) {
-        if (!sProyectos.existsById(id)) {
-            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
-        }
-        sProyectos.delete(id);
-        return new ResponseEntity(new Mensaje("preyecto eliminado"), HttpStatus.OK);
-    }
-    
-   
 
     }
     
--- src/main/java/com/portafolio/mgb/Dto/dtoProyectos.java
+++ src/main/java/com/portafolio/mgb/Dto/dtoProyectos.java
@@ -4,47 +4,11 @@
  */
 package com.portafolio.mgb.Dto;
 
-import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author Gabriel Habano
- */
 public class dtoProyectos {
     
    
-    @NotBlank
-    private String nombre;
-    @NotBlank
-    private String descripcion;
 
-    public dtoProyectos() {
-    }
-
-    public dtoProyectos(String nombre, String descripcion) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-  
-   
-   
     
     
 }
